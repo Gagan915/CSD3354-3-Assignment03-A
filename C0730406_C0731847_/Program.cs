359f0c1402d7de27855342e2679e1bb6e8026dd3
visualstudio <visualstudio@122-PC12>
2019-03-15 17:57:59 -0400

    commit4

--- C0730406_C0731847_/Program.cs
+++ C0730406_C0731847_/Program.cs
@@ -49,7 +49,7 @@ namespace C0730406_C0731847_
                 throw new Exception("Account frozen");
 
             }
-            if (amount>m_balance)
+            if (amount > m_balance)
             {
                 throw new ArgumentOutOfRangeException("amount");
             }
@@ -57,6 +57,37 @@ namespace C0730406_C0731847_
             {
                 throw new ArgumentOutOfRangeException("amount");
             }
+            m_balance += amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if(m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if(amount<0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+        public static void Main()
+        {
+            BankAccount ba new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Currnet balance is ${0}", ba.Balance);
         }
 
     }
